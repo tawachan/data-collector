2107c6d2240b19528910538bdcb90df64b5fc6a5
Yusuke Otawa / tawachan <otawa.yusuke@gmail.com>
2020-02-20 18:09:13 +0900

    swagger-ui用のjsonエンドポイントを作った

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,4 +50,13 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins '*'
+      resource '*',
+               headers: :any,
+               methods: %i[get post options head]
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@
 Rails.application.routes.draw do
   root to: 'application#index'
 
+  get 'apidoc', to: 'application#apidoc'
+
   namespace :api do
     namespace :twitter do
       resources :users, only: [:index]
