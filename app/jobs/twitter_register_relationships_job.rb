165fbb1138ce1a4c33ba73af4873f8d29ac5e071
Yusuke Otawa / tawachan <otawa.yusuke@gmail.com>
2020-02-24 17:29:15 +0900

    fix concurrency

--- app/jobs/twitter_register_relationships_job.rb
+++ app/jobs/twitter_register_relationships_job.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class TwitterRegisterRelationshipsJob < ApplicationJob
-  queue_as :twitter
+  queue_as :default
 
   # rubocop:disable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
   # layer_count の数だけ、再帰的にデータを取得していく
