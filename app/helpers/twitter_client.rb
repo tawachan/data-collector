36c4e7bcf6e911b5e74a1a32b5a26fb2ccf9dfa0
Yusuke Otawa / tawachan <otawa.yusuke@gmail.com>
2020-02-27 00:13:15 +0900

    fallbackいれた

--- app/helpers/twitter_client.rb
+++ app/helpers/twitter_client.rb
@@ -4,8 +4,16 @@ require 'twitter'
 
 class TwitterClient
   def initialize
-    @client = Twitter::REST::Client.new do |config|
-      key = !Rails.env.production? ? :production : :development
+    @client_development = Twitter::REST::Client.new do |config|
+      key = :development
+      config.consumer_key        = Rails.application.credentials[key][:twitter_api_key]
+      config.consumer_secret     = Rails.application.credentials[key][:twitter_secret_key]
+      config.access_token        = Rails.application.credentials[key][:twitter_access_token]
+      config.access_token_secret = Rails.application.credentials[key][:twitter_access_token_secret]
+    end
+
+    @client_production = Twitter::REST::Client.new do |config|
+      key = :production
       config.consumer_key        = Rails.application.credentials[key][:twitter_api_key]
       config.consumer_secret     = Rails.application.credentials[key][:twitter_secret_key]
       config.access_token        = Rails.application.credentials[key][:twitter_access_token]
@@ -14,12 +22,20 @@ class TwitterClient
   end
 
   def fetch_user(screen_name_or_id)
-    @client.user(screen_name_or_id)
+    default_client.user(screen_name_or_id)
+  rescue Twitter::Error::TooManyRequests
+    fallback_client.user(screen_name_or_id)
   end
 
   # 1度に最大5000件
   def fetch_friend_ids_of(screen_name, cursor = nil)
-    data = @client.friend_ids(screen_name, count: 5000, cursor: cursor).attrs
+    data = default_client.friend_ids(screen_name, count: 5000, cursor: cursor).attrs
+    ids = data[:ids]
+
+    ids = ids.concat(fetch_friend_ids_of(screen_name, data[:next_cursor])) unless data[:next_cursor].zero?
+    ids
+  rescue Twitter::Error::TooManyRequests
+    data = default_client.friend_ids(screen_name, count: 5000, cursor: cursor).attrs
     ids = data[:ids]
 
     ids = ids.concat(fetch_friend_ids_of(screen_name, data[:next_cursor])) unless data[:next_cursor].zero?
@@ -28,7 +44,12 @@ class TwitterClient
 
   # 1度に最大5000件
   def fetch_follower_ids_of(screen_name, cursor = nil)
-    data = @client.follower_ids(screen_name, count: 5000, cursor: cursor).attrs
+    data = default_client.follower_ids(screen_name, count: 5000, cursor: cursor).attrs
+    ids = data[:ids]
+    ids = ids.concat(fetch_follower_ids_of(screen_name, data[:next_cursor])) unless data[:next_cursor].zero?
+    ids
+  rescue Twitter::Error::TooManyRequests
+    data = fallback_client.follower_ids(screen_name, count: 5000, cursor: cursor).attrs
     ids = data[:ids]
     ids = ids.concat(fetch_follower_ids_of(screen_name, data[:next_cursor])) unless data[:next_cursor].zero?
     ids
@@ -37,6 +58,16 @@ class TwitterClient
   # 1度に最大100件
   # それ以上渡してもいい感じに複数回リクエスト飛ばしてくれるらしい
   def fetch_users(user_ids)
-    @client.users(user_ids)
+    default_client.users(user_ids)
+  rescue Twitter::Error::TooManyRequests
+    fallback_client.users(user_ids)
+  end
+
+  def default_client
+    Rails.env.production? ? @client_production : @client_development
+  end
+
+  def fallback_client
+    !Rails.env.production? ? @client_production : @client_development
   end
 end
